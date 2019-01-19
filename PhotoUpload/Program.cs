690aa3befc853485a19cf979e3b23d8c1cabb024
petrj <kirk@ncc1701>
2019-01-20 00:47:42 +0100

    CLI support

--- PhotoUpload/Program.cs
+++ PhotoUpload/Program.cs
@@ -16,17 +16,34 @@ namespace PhotoUpload
                 // workaround for mono linux System.Net.WebException: Error: TrustFailure (Authentication failed, see inner exception.) ---> System.Security.Authentication.AuthenticationException: Authentication failed, see inner exception. ---> Mono.Btls.MonoBtlsException: Ssl error:1000007d:SSL routines:OPENSSL_internal:CERTIFICATE_VERIFY_FAILED
                 return true;
             };
-    
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing DirectoryName argument");
+                return;
+            }
+
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too many arguments");
+                return;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.WriteLine($"Directory {args[0]} does not exist");
+                return;
+            }
+
             var photoUpload = new PhotoUpload();
             photoUpload.Connect();
 
             //photoUpload.UploadFolderToAlbum(new DirectoryInfo("/temp/PATHFIND"));
-            photoUpload.UploadFolders(new DirectoryInfo("/temp/"));
+            photoUpload.UploadFolders(new DirectoryInfo(args[0]));
 
             Logger.Info();
 
-            Console.WriteLine("Process finished. Press Enter.......");
-            Console.ReadLine();
-        }       
+            Console.WriteLine("Process finished.");
+        }
     }
 }
